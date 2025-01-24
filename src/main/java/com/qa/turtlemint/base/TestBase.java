c213d7b24146d669f8cf0ece5cbf06b9ba0c76dc
Turtlemint-QA Team <64896322+tm-qa@users.noreply.github.com>
2025-01-24 16:41:44 +0530

    yatra

--- src/main/java/com/qa/turtlemint/base/TestBase.java
+++ src/main/java/com/qa/turtlemint/base/TestBase.java
@@ -40,9 +40,9 @@ public class TestBase {
         if (browserName.equals("chrome")) {
             //driver = WebDriverManager.chromedriver().create();
             ChromeOptions options = new ChromeOptions();
-            options.setBrowserVersion("119");
+   //         options.setBrowserVersion("119");
 
-            driver = new ChromeDriver(options);
+ //           driver = new ChromeDriver(options);
 //            options.addArguments("start-maximized"); // open Browser in maximized mode
 //            options.addArguments("--incognito");
             String osName = System.getProperty("os.name");
@@ -63,7 +63,7 @@ public class TestBase {
 //				WebDriver driver = new ChromeDriver(options);
 
             }
-         //      driver = new ChromeDriver(options);
+               driver = new ChromeDriver(options);
 
             Dimension newDimension = new Dimension(1200, 800);
             driver.manage().window().setSize(newDimension);
@@ -77,8 +77,8 @@ public class TestBase {
             driver.manage().timeouts().pageLoadTimeout(TestUtil.Page_load_time, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtil.implicit_wait));
 
-            driver.get(prop.getProperty("url"));
-          //  driver.get(System.getProperty("url"));
+        //    driver.get(prop.getProperty("url"));
+            driver.get(System.getProperty("url"));
         }
 
     }
